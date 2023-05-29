25b9e55137ef4d5b9833a4f6e71857b0f9fb8115
kenethriera <kenethrr@outlook.com>
2023-05-29 10:53:28 -0500

    Se agregan ejemplo de uso de servicio

--- src/infrastructure/api/controllers/user_controller.rs
+++ src/infrastructure/api/controllers/user_controller.rs
@@ -1,23 +1,41 @@
 use crate::core::ports::user_port::IUserService;
 use actix_web::{get, post, web, HttpResponse, Responder};
+use serde::Serialize;
 use std::sync::Arc;
 
 type UserService = web::Data<Arc<dyn IUserService>>;
+
+#[derive(Serialize)]
+struct UserResponse {
+    name: String,
+    surname: String,
+}
 
 #[get("/users")]
 pub async fn get_users(user_service: UserService) -> impl Responder {
     let users = user_service.get_users().await.unwrap();
 
+    let mut response: Vec<UserResponse> = vec![];
     for user in users {
-        println!("User name: {}", user.name);
+        response.push(UserResponse {
+            name: user.name,
+            surname: user.surname,
+        });
     }
 
-    HttpResponse::Ok().body("get-users")
+    HttpResponse::Ok().json(response)
 }
 
 #[get("/users/{id}")]
-pub async fn get_user_by_id() -> impl Responder {
-    HttpResponse::Ok().body("get-user-by-id")
+pub async fn get_user_by_id(user_service: UserService) -> impl Responder {
+    if let Ok(user) = user_service.get_user_by_id(1).await {
+        HttpResponse::Ok().json(UserResponse {
+            name: user.name,
+            surname: user.surname,
+        })
+    } else {
+        HttpResponse::InternalServerError().body("Error")
+    }
 }
 
 #[post("/users")]
