c8ab53ef1fbc21f2499373e5b730aa42bad9cdd2
kenethriera <kenethrr@outlook.com>
2023-06-02 08:42:09 -0500

    Se agrega estructura para tests

---
+++ src/tests/core/services/user_service_stub.rs
@@ -0,0 +1,26 @@
+use crate::core::{entities::user::User, ports::user_port::IUserService};
+use async_trait::async_trait;
+
+struct UserServiceStub {}
+#[async_trait]
+impl IUserService for UserServiceStub {
+    async fn get_users(&self) -> Result<Vec<User>, anyhow::Error> {
+        Ok(vec![User {
+            id: Some(1),
+            name: "Keneth".to_string(),
+            surname: "Riera".to_string(),
+        }])
+    }
+    async fn get_user_by_id(&self, _id: u8) -> Result<User, anyhow::Error> {
+        unimplemented!()
+    }
+    async fn create_user(&self, _user: User) -> Result<(), anyhow::Error> {
+        unimplemented!()
+    }
+    async fn update_user(&self, _user_id: i32, _user: User) -> Result<(), anyhow::Error> {
+        unimplemented!()
+    }
+    async fn delete_user(&self, _user_id: i32) -> Result<(), anyhow::Error> {
+        unimplemented!()
+    }
+}
---
+++ src/tests/infrastructure/api/controllers/user_controller_test.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+
+    #[actix_web::test]
+    async fn test_get_users_ok() {
+        unimplemented!()
+    }
+}
