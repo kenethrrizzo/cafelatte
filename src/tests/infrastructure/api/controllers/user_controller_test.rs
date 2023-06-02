cc336b6f29c60423c82da69e6a6bddacdc1ee791
kenethriera <kenethrr@outlook.com>
2023-06-02 13:01:16 -0500

    Reorganización en pruebas unitarias

--- src/tests/infrastructure/api/controllers/user_controller_test.rs
+++ src/tests/infrastructure/api/controllers/user_controller_test.rs
@@ -5,12 +5,11 @@ mod tests {
         infrastructure::api::controllers::user_controller::get_users,
         tests::core::services::user_service_stub::UserServiceStub,
     };
-    use actix_web::{App, test, web};
+    use actix_web::{dev::ServiceResponse, test, web, App};
     use std::sync::Arc;
 
-    #[actix_web::test]
-    async fn test_get_users_ok() {
-        let user_service: Arc<dyn IUserService> = Arc::new(UserServiceStub { success: true });
+    async fn process_tests(success: bool) -> ServiceResponse {
+        let user_service: Arc<dyn IUserService> = Arc::new(UserServiceStub { success });
         let app = test::init_service(
             App::new()
                 .app_data(web::Data::new(user_service.clone()))
@@ -19,24 +18,18 @@ mod tests {
         .await;
 
         let req = test::TestRequest::get().uri("/users").to_request();
-        let resp = test::call_service(&app, req).await;
+        test::call_service(&app, req).await
+    }
 
+    #[actix_web::test]
+    async fn test_get_users_ok() {
+        let resp = process_tests(true).await;
         assert!(resp.status().is_success());
     }
 
     #[actix_web::test]
     async fn test_get_users_not_ok() {
-        let user_service: Arc<dyn IUserService> = Arc::new(UserServiceStub { success: false });
-        let app = test::init_service(
-            App::new()
-                .app_data(web::Data::new(user_service.clone()))
-                .route("/users", web::get().to(get_users)),
-        )
-        .await;
-
-        let req = test::TestRequest::get().uri("/users").to_request();
-        let resp = test::call_service(&app, req).await;
-
+        let resp = process_tests(false).await;
         assert!(resp.status().is_server_error());
     }
 }
